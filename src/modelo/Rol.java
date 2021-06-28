e173d619335e01f3c128d9ed2e772085f3e6ec16
jarbirS <61531454+jarbirS@users.noreply.github.com>
2021-06-28 09:21:05 -0500

    Add files via upload

--- src/modelo/Rol.java
+++ src/modelo/Rol.java
@@ -1,5 +1,63 @@
 package modelo;
 
-public class Rol {
+import java.io.Serializable;
+import javax.persistence.*;
+import java.util.List;
 
+@Entity
+@NamedQuery(name="Rol.findAll", query="SELECT r FROM Rol r")
+public class Rol implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+
+	private String description;
+
+	//bi-directional many-to-one association to Usuario
+	@OneToMany(mappedBy="rol")
+	private List<Usuario> usuarios;
+
+	public Rol() {
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return this.description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public List<Usuario> getUsuarios() {
+		return this.usuarios;
+	}
+
+	public void setUsuarios(List<Usuario> usuarios) {
+		this.usuarios = usuarios;
+	}
+
+	public Usuario addUsuario(Usuario usuario) {
+		getUsuarios().add(usuario);
+		usuario.setRol(this);
+
+		return usuario;
+	}
+
+	public Usuario removeUsuario(Usuario usuario) {
+		getUsuarios().remove(usuario);
+		usuario.setRol(null);
+
+		return usuario;
+	}
 }
